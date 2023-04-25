e729d846cec584d7aa0a15b56a623ffe6441784f
Wesley Rodrigues <108037910+WesleyRCampos@users.noreply.github.com>
2023-04-25 10:47:51 -0300

    v11

--- Projetos/Portifolio_Wesley copy/js/criptografar.js
+++
@@ -1,7 +0,0 @@
-const menubtn = document.getElementById('menu-btn');
-
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
-}
-menubtn.addEventListener('click', toggleMenu);
